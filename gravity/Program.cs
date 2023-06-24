8d69c49d2848da78438f46cb63ac8120a4de0d2c
Amir Hossein Amanipoor <76044261+Amirdtv@users.noreply.github.com>
2023-06-24 13:00:55 +0330

    g

---
+++ gravity/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gravity
+{
+    class Gravity
+    {
+        private int t;
+        double g = 9.8;
+        public int T
+        {
+            get { return t; }
+            set
+            {
+                if (value > 0)
+                {
+                    t = value;
+                }
+            }
+        }
+        public Gravity() { }
+        public Gravity(int T)
+        {
+            t = T;
+        }
+        public double result()
+        {
+            return 0.5 * g * t;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = Convert.ToInt32(Console.ReadLine());
+            Gravity g = new Gravity(T);
+            Console.WriteLine(" fasle = "+g.result());
+            Console.ReadKey();
+        }
+    }
+}
